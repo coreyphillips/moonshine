3d7f302943354d0920db1fa58e0c14a20e8e3d5b
Corey Phillips <corey.lyle.phillips@protonmail.com>
2020-04-13 10:26:01 -0400

    Updated updates array in Welcome.tsx.

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,12 +5,18 @@ import XButton from "./XButton";
 import { Text } from "../styles/components";
 
 const updates = [
-	`Dark Mode! This can be enabled from the Settings menu by toggling "Dark Mode" on.`,
-	"Fixed RBF button pop-in animation when transitioning to the Transaction Detail view."
+	"Upgraded to PSBT.",
+	"Removed slow or unresponsive Electrum servers from the default peer list.",
+	"Further optimized fees for Bech32 & Segwit-Compatible addresses.",
+	"Refresh the random peer list more frequently for more reliable connections.",
+	"Slight style updates to the RBF section of the transaction detail view.",
+	"Fixed BitId login functionality.",
+	"Blacklisted UTXO's are now labeled in the coin control modal.",
+	"Improved fee calculation when adding a transaction message."
 ];
 
 const _Welcome = ({ onClose = () => null, children = <View /> } = {}) => {
-	
+
 	if (Platform.OS === "ios") useEffect(() => LayoutAnimation.easeInEaseOut());
 	return (
 		<View style={styles.container}>
@@ -23,9 +29,9 @@ const _Welcome = ({ onClose = () => null, children = <View /> } = {}) => {
 				{children}
 				<Text style={[styles.subHeader, { textAlign: "center" }]}>Updates in this build include:</Text>
 				{updates.map((update, i) => <Text key={update} style={styles.text}><Text style={styles.semiBoldText}>{i+1}. </Text>{update}</Text>)}
-				
+
 				<Text style={styles.subHeader}>Questions?</Text>
-				
+
 				<Text style={styles.text}>Never hesitate to reach out:</Text>
 				<Text
 					onPress={() => Linking.openURL("mailto:support@moonshinewallet.com?subject=Requesting Some Help").catch((e) => console.log(e))}
